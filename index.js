dbbabb558b9b2cb914d6c801a4e05a6c2839345b
milos381 <milos_popovic@outlook.com>
2020-12-28 17:00:48 -0500

    commit tree 30c14f944dc07bcdab2d5eabd072d16a47160faf

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
@@ -9,6 +11,7 @@ function renderBooks(json) {
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
